eb0dfd54ac47fa75fd339f5bb67b934ed25e7341
Mohamed Machlou <medmachlou@gmail.com>
2024-08-17 18:29:33 +0100

    install mongoose and connect with mongoose

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 
+/// Use mongoose
+const db = process.env.DATABASE;
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch(() => {
+    console.log("database not  connect");
+  });
+
+///////////////////////////////////////////////////
+////////   Get Mathod  ////////////////////////////
+///////////////////////////////////////////////////
 app.get("/", (req, res) => {
   res.send({ message: "Working good ...." });
 });
 
+///////////////////////////////////////////////////
+////////   Port and Listen     ////////////////////
+///////////////////////////////////////////////////
 const port = process.env.PORT | 7000;
 app.listen(port, () => {
   console.log(`listen to this port : ${port}`);
