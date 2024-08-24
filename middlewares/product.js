153b724eccbfc49b2a2490aa0585ba99db4fc9ce
Mohamed Machlou <medmachlou@gmail.com>
2024-08-24 16:58:23 +0100

    get product by Id

---
+++ middlewares/product.js
@@ -0,0 +1,21 @@
+const Product = require("../models/product");
+
+exports.getProductById = (req, res, next, id) => {
+  Product.deleteOne();
+  Product.findById(id)
+    .then((product) => {
+      if (!product) {
+        return res.json({
+          error: "Product Not Found",
+        });
+      }
+      (req.product = product), next();
+    })
+    .catch((err) => {
+      if (err) {
+        return res.json({
+          error: "Product Not Found",
+        });
+      }
+    });
+};
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { createProduct } = require("../controllers/productController");
+const {
+  createProduct,
+  showProductById,
+} = require("../controllers/productController");
 const { requireSignIn, isAuth, isAdmin } = require("../middlewares/auth");
 const { userById } = require("../middlewares/user");
+const { getProductById } = require("../middlewares/product");
 
 router.post("/create/:userId", [requireSignIn, isAuth, isAdmin], createProduct);
 router.param("userId", userById);
+
+router.get("/:productId", showProductById);
+router.param("productId", getProductById);
 
 module.exports = router;
